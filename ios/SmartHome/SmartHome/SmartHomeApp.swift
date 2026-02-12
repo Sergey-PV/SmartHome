7c2d7f96d00c4f20df20ad876f4a6d7cdc25f3a2
Sergey <>
2026-02-12 18:07:48 +0300

    create ios project

---
+++ ios/SmartHome/SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Sergey on 12.02.26.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
